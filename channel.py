4269149961688d5802fb2e63ab6042a5e2e15e88
Unknown <maksimmerzh@gmail.com>
2017-12-08 23:55:58 +0300

    готово

---
+++ channel.py
@@ -0,0 +1,42 @@
+class Channel:
+    def __init__(self, service_time):
+        self._service_time = service_time  # время обслуживания
+        self.N = 0  # кол-во обработанных заявок
+        self._release_time = None
+        self._is_empty = True
+        self.queue = []
+
+    @property
+    def release_time(self):
+        return self._release_time
+
+    def set_service_time(self, service_time):
+        self._service_time = service_time
+
+    def _get(self):
+        item = self.queue.pop(0)
+        return item
+
+    def work(self, time):
+        item = None
+        if self.release_time == time:
+            item = self._get()
+            item += (self.release_time,)
+            self.N += 1
+            self._is_empty = True
+
+        if self.is_empty and self.queue:
+            self._is_empty = False
+            self._release_time = time + self._service_time  # начинает работать в тот же момент
+        return item
+
+    @property
+    def is_empty(self):
+        return self._is_empty
+
+    def add(self, item):
+        item_time = item[-1]
+        self.queue.append(item)
+        if self.is_empty:
+            self._is_empty = False
+            self._release_time = item_time + self._service_time
--- main.py
+++ main.py
@@ -1,20 +1,62 @@
-import random
-from queue import Queue
+from random import randint
+from channel import Channel
 
 interval = 8_000  # 8 sec
-x = random.randint(2, 8)  # время генерации
+x = randint(2, 8)  # время генерации
 n = 2  # количество каналов
 z = 4  # время обслуживания
 r = 3  # сниженное время обслуживания
 m = 10  # максимальное время нахождения в системе
 b = 0.27  # допустимый % потерь
 
-N = 0  # кол-во обработанных заявок
-X = 0  # кол-во отказов
-k1 = (False, 0)  # (is_busy, время освобождения)
-k2 = (False, 0)  # (is_busy, время освобождения)
-Q = Queue()  # [(номер, время_генерации),]
+N = 0  # кол-во переданных пакетов
+X = 0  # кол-во уничтоженных пакетов
 
-i = 0
+j = 0  # номер заявки
+t = 0  # время
+A = []  # [(j, i), ]  все заявки
+
+while (t + x) <= interval:
+    t += x
+    j += 1
+    A.append((j, t))
+    x = randint(2, 8)
+
+# REMOVE
+#========================================
+# import pickle
+# pickle.dump(A, open("save4.p", "wb"))
+# A = pickle.load(open("save3.p", "rb"))
+# j = len(A)
+# A_copy = list.copy(A)
+#========================================
+
+c1 = Channel(z)
+c2 = Channel(z)
+i = 0  # время
+
 while i < interval:
-    pass
+    if N and X/N > b:
+        c1.set_service_time(r)
+        c2.set_service_time(r)
+    else:
+        c1.set_service_time(z)
+        c2.set_service_time(z)
+
+    if A and A[0][1] == i:
+        c1.add(A[0])
+        A.pop(0)
+
+    w1 = c1.work(i)
+    if w1:
+        c2.add(w1)
+
+    w2 = c2.work(i)
+    if w2:
+        N += 1
+        time_diff = w2[-1] - w2[1]  # время передачи пакета
+        if time_diff > 10:
+            X += 1
+    i += 1
+
+print(j, N, X, X/N, sep="; ")
